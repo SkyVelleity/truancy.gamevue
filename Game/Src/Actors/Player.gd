929cb9e5e02bcc12129bdf656c04bfcfbf010a52
RMIT-Matt <95987351+RMIT-Matt@users.noreply.github.com>
2022-03-30 22:14:21 +1100

    Added Testwall01

--- Game/Src/Actors/Player.gd
+++ Game/Src/Actors/Player.gd
@@ -5,7 +5,7 @@ onready var raycast = $RayCast2D
 
 
 func _physics_process(delta: float) -> void:
-	
+	var move_vec = Vector2()
 	if Input.is_action_pressed("move_up"):
 		move_vec.y -= 1
 	if Input.is_action_pressed("move_down"):
@@ -15,7 +15,6 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("move_right"):
 		move_vec.x += 1
 	
-	var move_vec = Vector2()
 	move_vec = move_vec.normalized()
 	move_and_collide(move_vec * MOVE_SPEED * delta)
 	
