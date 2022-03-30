799caa160895d58b4b73ff28953ce96706b20ed0
Sky <velleity@outlook.com.au>
2022-03-30 23:15:07 +1100

    Migration to a new repo for more effective collaboration

--- Game/Src/Actors/Actor.gd
+++
@@ -1,7 +0,0 @@
-extends KinematicBody2D
-#Planning on making the player and enemies all connect to this 
-#Actor class later, right now just extending the movespeed and
-#kinematicbody
-class_name ActorBase
-
-const MOVE_SPEED = 300
